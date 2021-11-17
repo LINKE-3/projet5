912b360d8c37c536cb819725cd0826487104920c
LINKE-3 <71629466+LINKE-3@users.noreply.github.com>
2021-11-17 16:47:11 +0100

    Update CATEGORIES.py

--- CATEGORIES.py
+++ CATEGORIES.py
@@ -5,5 +5,5 @@ Created on Wed Jun 23 18:38:53 2021
 @author: alain
 """
 """category liste"""
-CATEGORIES = [(1, "pate à tartiner"), (2, "chocolat"),
+CATEGORIES = [(1, "pain"), (2, "chocolat"),
               (3, "pizza"), (4, "crêpe"), ]
